a6dbb00c330621444c40a5ac6605afcfa74464d3
Giovani-Coelho <giovanicoelho2004@hotmail.com>
2023-05-30 16:29:28 -0300

    Diagonal Difference

---
+++ src/Hackerrank/DiagonalDifference.java
@@ -0,0 +1,27 @@
+package Hackerrank;
+
+public class DiagonalDifference {
+  public static void main(String[] args) {
+    int[][] arr = new int[3][];
+    arr[0] = new int[]{11, 2, 4};
+    arr[1] = new int[]{4, 5, 6};
+    arr[2] = new int[]{10, 8, -12};
+
+    int arrLenght = arr.length - 1;
+    int index = 0;
+    int left = 0;
+    int right = 0;
+
+    for (int[] value : arr) {
+      left += value[index];
+      index++;
+    }
+
+    for (int[] value : arr) {
+      right += value[arrLenght];
+      arrLenght--;
+    }
+
+    System.out.println(Math.abs(left - right));
+  }
+}
